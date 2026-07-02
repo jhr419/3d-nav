eaf4b6e3c2e54a8e587c39b538cde995b92f8487
jhr419 <1160375083@qq.com>
2026-07-02 14:55:27 +0800

    调整setup.bash

--- setup.bash
+++ setup.bash
@@ -1,3 +1,3 @@
 unset ROS_DOMAIN_ID
-source /home/jhr/3dnav_ws/src/unitree_ros2/setup_default.sh
-source ~/3dnav_ws/install/setup.bash
+source ./src/robot_api/unitree_ros2/setup_default.sh
+source ./install/setup.bash
